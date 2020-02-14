bd345d254430d385450dc5fca6b3e366e83db0ec
Mario Valenzuela <mariovalpa@gmail.com>
2020-02-14 00:35:14 -0700

    Se implementa batch normalization dentro de modulo inception y overlapping pooling, muy buenos resultados 85% Cifar10

--- cifar10_test2.py
+++ cifar10_test2.py
@@ -40,7 +40,7 @@ model.summary()
 
 optimizer = tf.keras.optimizers.RMSprop(learning_rate=0.001, decay=1e-6)
 
-model.compile(optimizer=optimizer,
+model.compile(optimizer="adam",
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
